b4612b542d48f3e861163cd2fd6d95c46c2c2c8d
Jurabek <jorabeksindarov44@gmail.com>
2024-01-26 22:14:45 +0500

    Task domain models created

--- TaskManagementSystem.Domain/Models/Tasks/Tasks.cs
+++ TaskManagementSystem.Domain/Models/Tasks/Tasks.cs
@@ -2,5 +2,17 @@ namespace TaskManagementSystem.Domain.Models.Tasks;
 
 public class Tasks
 {
-    
+    public Guid Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTimeOffset DueDate { get; set; }
+
+    public string TaskPriority { get; set; }
+
+    public string state { get; set; }
+
+    public string Note { get; set; }
 }
